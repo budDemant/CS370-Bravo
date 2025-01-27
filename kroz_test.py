38d497b3e74ffbdaa32a1e16323c003de0920923
Anthony DeMatteo <anthde13@gmail.com>
2025-01-26 22:44:12 -0500

    - Created class Object. This will make the code more consistent as we make more objects (which inherit Object) in the game (Gems, Creatures, Player, etc.). - Created class Wall. It does not stop the player at this moment, but it draws a rectangle of a given color. - Changed from global variables to dictionary in init_vars() because it could lead to issues if player_position is changed in game loop.

--- kroz_test.py
+++ kroz_test.py
@@ -27,20 +27,68 @@ BLACK = pygame.Color(0, 0, 0)
 # green = pygame.Color(0, 255, 0)
 # blue = pygame.Color(0, 0, 255)
 YELLOW = pygame.Color(255, 255, 0)
+BROWN = pygame.Color(125, 50, 0)
 
 
-# Circle properties
-circle_radius = 8 # size of player object
+# Player properties
+global player_step
 player_step = 20 # player moves 20 pixels for each step
 
 # Function to initialize game variables
-def init_vars():
-    global player_position, direction
-    player_position = [screen_width // 2, screen_height // 2]
-    direction = "UP"
+def init_vars(): 
+    return {
+        "player_position": [screen_width // 2, screen_height // 2],
+        "player_color": YELLOW,
+        "player_size": 8, # radius of circle
+        "direction": "UP"
+    }
 
 # Initialize game variables
-init_vars()
+game_state = init_vars()
+
+# this class will also need a score and message hint attributes
+class Object:
+    def __init__(self, position, size, color):
+        self.position = position
+        self.size = size
+        self.color = color
+    
+    def draw(self, screen):
+        pass # because it will be overwritten by children classes
+    
+class Player(Object):
+    def __init__(self, position, size, color):
+        super().__init__(position, size, color)
+    
+    def draw(self, screen):
+        for pos in self.position:
+            pygame.draw.circle(screen, self.color, self.position, self.size)
+
+class Wall(Object):
+    def __init__(self, position, size, color):
+        super().__init__(position, size, color)
+        self.rect = pygame.Rect(position[0], position[1], size[0], size[1])
+    
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.color, self.rect)
+        
+
+
+# damage, speed, graphics, 2 states of behavior (idle and chase)
+# class Enemy(Object):
+    
+# class Gem(Object):
+    
+# class Whip(Object):
+        
+player = Player(game_state["player_position"], game_state["player_size"], game_state["player_color"])
+        
+
+wall_position = [screen_width // 4, screen_height // 4]
+wall_size = [20,50]
+wall_color = BROWN
+    
+wall_1 = Wall(wall_position, wall_size, wall_color)
 
 
 # Game loop
@@ -54,25 +102,27 @@ while True:
         # >= 0 to avoid negative position values
         # <= screen_height/width to prevent player from moving off the screen
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP and player_position[1] - player_step >= 0:
-                player_position[1] -= player_step # moves player up by 20 pixels
-            elif event.key == pygame.K_DOWN and player_position[1] - player_step <= screen_height:
-                player_position[1] += player_step
-            elif event.key == pygame.K_LEFT and player_position[0] - player_step >= 0:
-                player_position[0] -= player_step
-            elif event.key == pygame.K_RIGHT and player_position[0] and player_step <= screen_width:
-                player_position[0] += player_step
+            if event.key == pygame.K_UP and game_state["player_position"][1] - player_step >= 0:
+                game_state["player_position"][1] -= player_step # moves player up by 20 pixels
+            elif event.key == pygame.K_DOWN and game_state["player_position"][1] - player_step <= screen_height:
+                game_state["player_position"][1] += player_step
+            elif event.key == pygame.K_LEFT and game_state["player_position"][0] - player_step >= 0:
+                game_state["player_position"][0] -= player_step
+            elif event.key == pygame.K_RIGHT and game_state["player_position"][0] - player_step <= screen_width:
+                game_state["player_position"][0] += player_step
                 
     
 
-    
     # Graphics
+    
+    # Draw background
     screen.fill(BLACK)
     
-    for pos in player_position:
-        pygame.draw.circle(screen, YELLOW, player_position, circle_radius)
+    # Draw Player
+    player.draw(screen)
     
-        
+    # Draw Wall
+    wall_1.draw(screen)  
     
     # Update the display
     pygame.display.flip()

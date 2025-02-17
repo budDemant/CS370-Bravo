65e88aa739431da5b0b98f3d58e54330784526b9
Anthony DeMatteo <anthde13@gmail.com>
2025-02-17 10:07:24 -0500

    - kroz.py: error checker - kroz.py: window caption

--- kroz.py
+++ kroz.py
@@ -1,9 +1,20 @@
 import pygame, sys
 
+
+# Initialize Pygame and check for errors
+check_errors = pygame.init()
+#check_errors[1] is the second tuple of pygame.init() which contains number of errors
+if check_errors[1] > 0:
+    print("Error " + check_errors[1])
+else:
+    print("Game Successfully initialized")
+
+
 screen_width = 640
 screen_height = 480
 
 screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("Kroz")
 
   
 while True:

f0a43e469371dd60829fd55c72ea721422f7c96f
TheLobster291 <jbernatavitz@gmail.com>
2025-02-24 15:18:43 -0500

    added teleport sprite. Removed purple color

--- entities/teleport.py
+++ entities/teleport.py
@@ -1,11 +1,10 @@
-from constants import PURPLE
 from entities.player import Player
 from renderer.cell import Cell
 
 class Teleport(Cell):
     def __init__(self) -> None:
         super().__init__()
-        self.image.fill(PURPLE)
+        self.load_sprite("./sprites/teleport.png")
 
     def on_collision(self, cell: "Cell") -> bool:
         if isinstance(cell, Player):

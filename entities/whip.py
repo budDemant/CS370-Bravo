b6700dcb9ddd679a079f67e37a9660ca907a6e50
Eva <morgune@sunypoly.edu>
2025-03-29 21:45:31 -0400

    creating a whip

--- entities/whip.py
+++ entities/whip.py
@@ -7,15 +7,48 @@ class Whip(Cell):
         super().__init__()
         self.col(15, 7)
         self.load_dos_char(244)
+        
+    def use_whip(self):
+        if self.whip_count > 0:
+            self.whip_count -= 1
+            #whip_sound.play() 
+            return True
+        return False
 
     def on_collision(self, cell: "Cell") -> bool:
         if isinstance(cell, Player):
             print("Im indiana jonesing it!!!!!")
+            #if self.whip_count > 0:
+                #self.whip_count -= 1
+                #whip_sound.play() 
+                #eturn True
+            #return False
             #from level.level_load import game_instance
             #if game_instance:
                 #game_instance.whip_count += 1
             return True
         return False
+    
+    '''def activate(self):
+        if self.player.use_whip():
+            hits = []
+            if self.player.y > 100 and self.player.x > 100:
+                hits.append((self.player.x - 1, self.player.y - 1, '\\'))
+            if self.player.x > 100:
+                hits.append((self.player.x - 1, self.player.y, 'ƒ'))
+            if self.player.y < 500 and self.player.x > 100:
+                hits.append((self.player.x - 1, self.player.y + 1, '/'))
+            if self.player.y < 500:
+                hits.append((self.player.x, self.player.y + 1, '≥'))
+            if self.player.y < 500 and self.player.x < 700:
+                hits.append((self.player.x + 1, self.player.y + 1, '\\'))
+            if self.player.x < 700:
+                hits.append((self.player.x + 1, self.player.y, 'ƒ'))
+            if self.player.y > 100 and self.player.x < 700:
+                hits.append((self.player.x + 1, self.player.y - 1, '/'))
+            if self.player.y > 100:
+                hits.append((self.player.x, self.player.y - 1, '≥'))'''
+                # Need to implement pressing w 
 
     #TODO: get player count of whips
         #button to use whip

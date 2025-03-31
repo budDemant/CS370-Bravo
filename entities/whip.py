954e412914c53cc2f72d773803ab96472b09fd40
Eva <morgune@sunypoly.edu>
2025-03-31 13:36:59 -0400

    CountFactorFixed

--- entities/whip.py
+++ entities/whip.py
@@ -8,16 +8,29 @@ class Whip(Cell):
         self.col(15, 7)
         self.load_dos_char(244)
         
-    def use_whip(self):
+    '''def use_whip(self):
+        print(f"Whip count before use: {self.whip_count}")  
         if self.whip_count > 0:
             self.whip_count -= 1
-            #whip_sound.play() 
+            print(f"Whip count after use: {self.whip_count}")  
             return True
-        return False
+        print("No whips left!")
+        return False'''
 
     def on_collision(self, cell: "Cell") -> bool:
         if isinstance(cell, Player):
-            print("Im indiana jonesing it!!!!!")
+            print("I'm Indiana Jonesing it!!!!!") 
+            from level.level_load import game_instance 
+            if game_instance.whip_count > 0:
+                if game_instance:
+                    game_instance.whip_count += 1   
+                    return True
+                else:
+                    game_instance.whip_count -= 1 
+            else:
+                print("No whips left!")
+
+        return False 
             #if self.whip_count > 0:
                 #self.whip_count -= 1
                 #whip_sound.play() 
@@ -26,9 +39,7 @@ class Whip(Cell):
             #from level.level_load import game_instance
             #if game_instance:
                 #game_instance.whip_count += 1
-            return True
-        return False
-    
+
     '''def activate(self):
         if self.player.use_whip():
             hits = []
@@ -47,8 +58,8 @@ class Whip(Cell):
             if self.player.y > 100 and self.player.x < 700:
                 hits.append((self.player.x + 1, self.player.y - 1, '/'))
             if self.player.y > 100:
-                hits.append((self.player.x, self.player.y - 1, '≥'))'''
-                # Need to implement pressing w 
+                hits.append((self.player.x, self.player.y - 1, '≥'))
+                # Need to implement pressing w '''
 
     #TODO: get player count of whips
         #button to use whip

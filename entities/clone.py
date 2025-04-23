24279edc5f8243677dce40c2ac85370acb779165
Anthony DeMatteo <anthde13@gmail.com>
2025-04-23 14:51:43 -0400

    clone copy because the old branch can't be rebased (due to various merge conflicts).

---
+++ entities/clone.py
@@ -0,0 +1,23 @@
+from constants import WHITE
+from entities.player import Player
+from renderer.cell import Cell
+from renderer.cell_grid import CellGrid
+
+def spawn_random_player(grid: CellGrid):
+    x, y = grid.get_random_empty_tiles()
+    grid.put((x, y), Player())
+
+class Clone(Cell):
+    def __init__(self) -> None:
+        super().__init__()
+        self.col(2, 7)
+        self.load_dos_char(21)
+
+    def on_collision(self, cell: "Cell") -> bool:
+        if isinstance(cell, Player) and self.grid:
+            print('Wow, you just cloned yourself!')
+            spawn_random_player(self.grid)
+        
+            return True
+        return False
+

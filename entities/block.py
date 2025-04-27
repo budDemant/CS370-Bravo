cacb2533f6072c3ea3b59e5cfdb00f73deff6027
Anthony DeMatteo <anthde13@gmail.com>
2025-04-27 10:17:09 -0400

    Enemies now die when they move into crumbled walls!!!

--- entities/block.py
+++ entities/block.py
@@ -1,6 +1,7 @@
 from renderer.cell import Cell
 from entities.player import Player
 from Sound import SoundEffects 
+from entities.enemy import Enemy
 
 
 class Block(Cell):
@@ -18,6 +19,7 @@ class Block(Cell):
     def is_breakable_wall(self): return True
 
     def on_collision(self, cell: "Cell") -> bool:
+        
         if isinstance(cell, Player):
             grid = self.grid
             grid.flash(18,25,'A Crumbled Wall blocks your way.')
@@ -27,4 +29,12 @@ class Block(Cell):
                 if game_instance.score > 20:
                     game_instance.score -= 20
         self.sound_effects.play_in_thread(self.sound_effects.BlockSound, True)
+        
+        if isinstance(cell, Enemy): # Enemy dies when it moves into Block
+            print("Enemy and Block destroyed!")
+            if cell.grid:
+                cell.grid.remove((cell.x, cell.y))  # Remove Enemy
+            if self.grid:
+                self.grid.remove((self.x, self.y))  # Remove Block
+            return False
         return False

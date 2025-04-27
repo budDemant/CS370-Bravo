69a705e0f2f17bd4e8a1d275b611caa073a44476
Anthony DeMatteo <anthde13@gmail.com>
2025-04-27 12:08:05 -0400

    Somehow 2 level data characters were switched when importing them in level 19.

--- game.py
+++ game.py
@@ -73,7 +73,7 @@ class Game:
         self.gem_color, self.art_color = new_gem_color()
 
         self.difficulty = 8
-        self.current_level = 1
+        self.current_level = 19
         self.color = True
         self.fastpc = True
 

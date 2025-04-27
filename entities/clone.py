ef6ad37570766943a71f0b4be7aadc146f449f9d
Anthony DeMatteo <anthde13@gmail.com>
2025-04-26 22:35:05 -0400

    Clone is now green instead of yellow.

--- entities/clone.py
+++ entities/clone.py
@@ -1,22 +1,29 @@
-from pygame import Color
-from constants import LIGHTGRAY
+from pygame import Surface, Color
+from constants import LIGHTGREEN
 from entities.player import Player
 from renderer.cell import Cell
 
 class Clone(Player):
     def __init__(self) -> None:
         super().__init__()
-        self.col(2, 7)  # Make the clone green
+
         self.clone = True
+
+        # Redefine the appearance
+        self.fg = (Color(0, 255, 0), Color(0, 255, 0))  # green in color & monochrome
+        self.bg = (Color(0, 0, 0, 0), Color(0, 0, 0, 0))  # transparent background
+
+        # Recreate the visual appearance
+        self.load_dos_char(2)  # Re-load the same DOS char but with new fg/bg
 
     def use_whip(self) -> None:
         """Prevent clone from whipping."""
-        pass  # Disable whip usage
+        pass
 
     def on_collision(self, cell: "Cell") -> bool:
         from entities.enemy import Enemy
         from entities.lava import Lava
-        
+
         if isinstance(cell, (Enemy, Lava)):
             if self.grid:
                 print("Clone removed!")
@@ -24,4 +31,3 @@ class Clone(Player):
             return True
 
         return super().on_collision(cell)
-

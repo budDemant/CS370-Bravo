14f753c13d179870c1697fa827aad90832d55806
Eva <morgune@sunypoly.edu>
2025-04-28 11:22:15 -0400

    Kroz Letters working

--- entities/kroz_letter.py
+++ entities/kroz_letter.py
@@ -8,25 +8,73 @@ class KLetter(Cell):
         super().__init__()
         self.col(14, 7)
         self.load_dos_char(75)
-        self.fast_pc = False
+        
+    def on_collision(self, cell: "Cell") -> bool:
+        if isinstance(cell, Player):
+            print("Player hit a Kroz Letter!")
+        
+            #self.sound_effects.play_in_thread(self.sound_effects.GrabSound, self.fast_pc)
+        
+            from level.level_load import game_instance
+            if game_instance:
+                game_instance.score += 2000
+                print (game_instance.score)
+                return True
+        return False
+
 
 class RLetter(Cell):
     def __init__(self) -> None:
         super().__init__()
         self.col(14, 7)
         self.load_dos_char(82)
-        self.fast_pc = False
+        
+    def on_collision(self, cell: "Cell") -> bool:
+        if isinstance(cell, Player):
+            print("Player hit a Kroz Letter!")
+        
+            #self.sound_effects.play_in_thread(self.sound_effects.GrabSound, self.fast_pc)
+        
+            from level.level_load import game_instance
+            if game_instance:
+                game_instance.score += 2000
+                return True
+        return False
+
 
 class OLetter(Cell):
     def __init__(self) -> None:
         super().__init__()
         self.col(14, 7)
         self.load_dos_char(79)
-        self.fast_pc = False
+        
+    def on_collision(self, cell: "Cell") -> bool:
+        if isinstance(cell, Player):
+            print("Player hit a Kroz Letter!")
+        
+            #self.sound_effects.play_in_thread(self.sound_effects.GrabSound, self.fast_pc)
+        
+            from level.level_load import game_instance
+            if game_instance:
+                game_instance.score += 2000
+                return True
+        return False
+
 
 class ZLetter(Cell):
     def __init__(self) -> None:
         super().__init__()
         self.col(14, 7)
         self.load_dos_char(90)
-        self.fast_pc = False
+        
+    def on_collision(self, cell: "Cell") -> bool:
+        if isinstance(cell, Player):
+            print("Player hit a Kroz Letter!")
+        
+            #self.sound_effects.play_in_thread(self.sound_effects.GrabSound, self.fast_pc)
+        
+            from level.level_load import game_instance
+            if game_instance:
+                game_instance.score += 2000
+                return True
+        return False
